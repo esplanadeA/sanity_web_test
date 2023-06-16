917172046c0582e271a01e7e6ea87f154d968cf0
esplanadeA <anna.li.dev@gmail.com>
2023-06-17 00:13:05 +1200

    Navbar links added

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,18 @@ import { images } from '../../constants/index';
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={images.logo} alt="logo"></img>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
