f7de479247c43d62422131a1a637d232b42a27af
esplanadeA <anna.li.dev@gmail.com>
2023-06-18 11:00:28 +1200

    react toolkit added

--- frontend_react/src/containers/Skills/Skills.jsx
+++ frontend_react/src/containers/Skills/Skills.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import { motion } from 'framer-motion';
+import { images } from '../../constants/index';
+import { urlFor, client } from '../../client';
+
+import AppWrap from '../../wrapper/AppWrap';
+
 import './Skills.scss';
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <>
+      <h2 className="head-text">Skills</h2>
+    </>
+  );
 };
 
 export default Skills;
