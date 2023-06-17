f93c51d9eef19eb1396d105873f80b0f59c19521
esplanadeA <anna.li.dev@gmail.com>
2023-06-17 18:55:33 +1200

    side navigation bar setup with map feature

---
+++ frontend_react/src/components/NavigationDots.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const NavigationDots = (active) => {
+  return (
+    <div className="app__navigation">
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
+        (item, index) => (
+          <a
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: '#313BAC' } : {}}
+            href={`#${item}`}
+            key={item + index}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default NavigationDots;
