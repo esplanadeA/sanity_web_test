01a806923e0ddbe71d70f416ab86828079602459
esplanadeA <anna.li.dev@gmail.com>
2023-06-18 18:12:47 +1200

    adding catch error

--- frontend_react/src/containers/Footer/Footer.jsx
+++ frontend_react/src/containers/Footer/Footer.jsx
@@ -31,10 +31,13 @@ const Footer = () => {
       message: message,
     };
 
-    client.create(contact).then(() => {
-      setLoading(false);
-      setIsFormSubmitted(true);
-    });
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -42,8 +45,19 @@ const Footer = () => {
       <h2 className="heat-text">Take A coffe & chat with me</h2>
       <div className="app__footer-cards">
         <div className="app__footer-card">
+          <img src={images.email} alt="location" />
+          <a
+            href="https://www.google.com/maps/place/Tauranga/@-37.7539917,176.0690809,12z/data=!3m1!4b1!4m6!3m5!1s0x6d6e76e77bd464e5:0x500ef6143a39926!8m2!3d-37.6869653!4d176.1654272!16zL20vMDFuNmx6?entry=ttu"
+            target="_blank"
+            className="p-text"
+            rel="noreferrer"
+          >
+            Tauranga New Zealand
+          </a>
+        </div>
+        <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="anna.li.dev@gmail.com" className="p-text">
+          <a href="mailto:anna.li.dev@gmail.com" className="p-text">
             anna.li.dev@gmail.com
           </a>
         </div>
